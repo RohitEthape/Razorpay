b5b1ebb4498ad4d5d4ba1d1e599b34b52359727a
Rohit Ethape <ethaperohit441@gmail.com>
2026-03-15 23:17:33 +0530

    all services created

---
+++ fraud-service/fraud-service/src/test/java/com/razorpay/fraudservice/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razorpay.fraudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/notification-service/src/test/java/com/razorpay/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razorpay.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
